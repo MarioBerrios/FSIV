d170f36c0c5573f3a61cb25764ea3ae9cdc93a18
Mario Berrios Carmona <i12bercm@uco.es>
2022-10-25 18:13:19 +0200

    some changes to p3e2 circular expansion

---
+++ p3/sharpening/build/CMakeFiles/sharpen.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sharpen.dir/common_code.cpp.o"
+  "CMakeFiles/sharpen.dir/common_code.cpp.o.d"
+  "CMakeFiles/sharpen.dir/sharpen.cpp.o"
+  "CMakeFiles/sharpen.dir/sharpen.cpp.o.d"
+  "sharpen"
+  "sharpen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sharpen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
